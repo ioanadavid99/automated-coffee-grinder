33e209037a9399953b6c554004bddd375aacad58
Ioana David <34715226+ioanadavid99@users.noreply.github.com>
2020-09-21 23:26:29 -0700

    adding wifi support for two nodemcus

---
+++ wifi-stuff/wifi_server_01/wifi_server_01.ino
@@ -0,0 +1,64 @@
+/*  Connects to the home WiFi network
+ *  Asks some network parameters
+ *  Starts WiFi server with fix IP and listens
+ *  Receives and sends messages to the client
+ *  Communicates: wifi_client_01.ino
+ *  
+ *  author: https://www.instructables.com/id/WiFi-Communication-Between-Two-ESP8266-Based-MCU-T/
+ *  
+ *  I have made some modifications to the code for my own purposes. 
+ */
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+
+byte ledPin = 2;
+// removed SSID and password for security reasons 
+char ssid[] = "";
+char pass[] = ""; 
+WiFiServer server(80);                    
+
+IPAddress ip(192, 168, 0, 201);               // IP address of the server -> of the MCU 
+IPAddress gateway(192,168,0,1);             // gateway of your network - wifi router 
+IPAddress subnet(255,255,255,0);            // subnet mask of your network
+
+void setup() {
+  Serial.begin(115200);                   // only for debug
+  WiFi.config(ip, gateway, subnet);       // forces to use the fix IP
+  WiFi.begin(ssid, pass);                 // connects to the WiFi router
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+  /*Serial.print("Status: "); Serial.println(WiFi.status());    // Network parameters
+  Serial.print("IP: ");     Serial.println(WiFi.localIP());
+  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());*/ 
+    delay(500);
+  }
+  //Serial.print("server is connected"); 
+  server.begin();                         // starts the server
+  Serial.println("Connected to wifi");
+  Serial.print("Status: "); Serial.println(WiFi.status());    // Network parameters
+  Serial.print("IP: ");     Serial.println(WiFi.localIP());
+  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop () {
+  WiFiClient client = server.available();
+  if (client) {
+    if (client.connected()) {
+      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      Serial.println(".");
+      String request = client.readStringUntil('\r');    // receives the message from the client
+      Serial.print("From client: "); Serial.println(request);
+      client.flush();
+      client.println("Hi client! No, I am listening.\r"); // sends the answer to the client
+      digitalWrite(ledPin, HIGH);
+    }
+    client.stop();                // tarminates the connection with the client
+  }
+}

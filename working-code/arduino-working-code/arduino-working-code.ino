6754b45e02fe2f084da1c8300d6b26538314ccb6
Ioana David <34715226+ioanadavid99@users.noreply.github.com>
2020-10-31 20:20:22 -0700

    update arduino-working-code for additional functionality, currently testing

--- working-code/arduino-working-code/arduino-working-code.ino
+++ working-code/arduino-working-code/arduino-working-code.ino
@@ -1,6 +1,6 @@
 /*
  * Author:        Ioana David
- * Last modified: oCT. 12 2020 
+ * Last modified: OCT. 12 2020 
  * Purpose:       Beginning to put different components of the project together. See GitHub for a detailed explanation of the process. This code will be downloaded to an Arduino Uno. 
  * 
  * Credits:
@@ -18,7 +18,17 @@
  #define  CLK    2    // this will also probably have to change, keeping it for now 
  #define  CALIBRATION_FACTOR  242   // this is the calibration factor determined from tests using the author's code; can be changed later if necessary 
 
+ // other definitions 
+ #define  BASE_WEIGHT     273   // weight in grams of the coffee grinder WITHOUT any coffee 
+
+// error codes here for reading the weight of the coffee grinder 
+ #define  SUCCESS         0
+ #define  ERR_LOW_COFFEE  1
+ #define  ERR_NO_COFFEE   2
+ #define  ERR_UNKNOWN     3
+
  HX711 scale; 
+ int scaleStatus;     // this is the status of the scale. global var - treating it like a status register in an MCU. 
   
 void setup() {
   Serial.begin(115200);   // the baud rate for the serial monitor for easier debugging 
@@ -29,9 +39,13 @@ void setup() {
   scale.tare(); 
 
   // other configuration thingies below 
+
+  // for debugging 
+  pinMode(LED_BUILTIN, OUTPUT); 
 }
 
 void loop() {
+
   // go to deep sleep immediately
   // if you get an interrupt, wake up, and weigh the scale
   // if it is low, WARN the user that there may not be enough coffee for the amount that they want to grind
@@ -39,36 +53,105 @@ void loop() {
   // else, send the command to the motor to grind for X seconds to achieve desired amount of ground coffee 
   // it grinds. then it goes back to sleep. tada!
 
-  goingToSleep();
+  digitalWrite(LED_BUILTIN, LOW); 
+  goingToSleep();   // this function just makes sure it goes to sleep. then it basically calls other ISRs when it wakes up. it *doesn't* have to be implemented as ISRs (ie. the code could all be functions) but i wanted to test out using ISRs for deep sleep versus just normal functions 
 } 
 
 /*
  * Purpose:     puts the Arduino in deep sleep mode unless interrupted otherwise 
  * Parameters:  none
+ * Return:      none 
  */
 void goingToSleep(void) { 
   Serial.println("going to sleep... Zzz"); 
+  digitalWrite(LED_BUILTIN, LOW);       // for debugging 
   
   // not sure if this will work - different library, unfamiliar with it 
-  LowPower.deepSleep();       // apparently puts the Arduino in deep sleep mode - using a different library so not sure 
+  //LowPower.deepSleep();       // apparently puts the Arduino in deep sleep mode - using a different library so not sure 
   
   // alternatively (from the tutorial):
   sleep_enable(); 
   attachInterrupt(digitalPinToInterrupt(2), wakeUp, HIGH);   // setting an interrupt for the NodeMCU 
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
-  sleep_cpu(); 
+  sleep_cpu();  // this enables sleep mode again 
   Serial.println("just woke up! :D"); 
 }
 
+/*
+ * Purpose:     wakes the Arduino up to get the reading from the scale 
+ * Parameters:  none
+ * Return:      none   
+ */
 void wakeUp(void) { 
   sleep_disable(); 
-  detachInterrupt(digitalPinToInterrupt(2));          // tbh still not too sure what this does?? 
+  digitalWrite(LED_BUILTIN, HIGH);                    // for debugging 
+  detachInterrupt(digitalPinToInterrupt(2));          // tbh still not too sure what this does?? removes the interrupt from pin2... might not operate as a loop in this case 
+
+  // for debugging 
+  /*
+  checkWeight();    // get the scale to do its thing 
+  if(scaleStatus == SUCCESS) { 
+    grindCoffee(1);  // TODO: need to get the serving num from the nodemcu here 
+  }
+
+  else{ 
+    triggerFault();       // functionality for edge cases has not yet been determined -- just triggering a fault or something like that 
+  }
+  */
+ 
+} 
+
+/*
+ * Purpose:     checks the scale and updates the status register as appropriate  
+ * Parameters:  none
+ * Return:      none 
+ */ 
+void checkWeight(void){ 
+    // TODO: power up the scale -- verify 
+  scale.power_up();     // saves the life of the battery-powered scale :-) 
   
   // the most basic thing to do: get the scale to perform a reading 
+  // serial monitor for debugging right now 
   Serial.print("reading from the scale: ");
   Serial.print(scale.get_units(), 1); 
   Serial.print("g"); 
   Serial.println(); 
 
-  // TODO: tell the NodeMCU that it's done grinding. 
+  int weight = scale.get_units(); 
+
+  // error codes to store in scaleStatus (like a status register) 
+  if (weight = BASE_WEIGHT) { 
+    scaleStatus = ERR_NO_COFFEE; 
+  }
+  else if(weight <= (BASE_WEIGHT + 5)) { 
+    scaleStatus = ERR_LOW_COFFEE; 
+  }
+  else if(weight < BASE_WEIGHT) { 
+    scaleStatus = ERR_UNKNOWN; 
+  }
+  else { 
+    scaleStatus = SUCCESS; 
+  }
+
+  // TODO: power down the scale -- verify 
+  scale.power_down();   
+}
+
+// TODO: finish function that tells the motor to grind
+/*
+ * Purpose:     tells the motor attached to the Arduino to grind coffee for a desired serving 
+ * Parameters:  servingNum: the serving number (eg. one cup, two cups) 
+ * Return:      none 
+ */ 
+void grindCoffee(int servingNum) {    // could also get the serving num from inside the function to make it more like an ISR? 
+  while(1);   // just for debugging 
+}
+
+/*
+ * Purpose:     functionality for dealing with edge cases 
+ * Parameters:  none
+ * Return:      none 
+ */ 
+void triggerFault(void) { 
+  while(1);   // just stay here for now... will figure out additional functionality in the future 
 }
